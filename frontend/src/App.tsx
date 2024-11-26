bc5f98d530115bdcc8e2ae1afcfc7260eca992d6
Prince-Lartey <lartey.princ@gmail.com>
2024-11-26 18:52:43 +0000

    Implemented the add hotel functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register"
 import SignIn from "./pages/SignIn"
 import AddHotel from "./pages/AddHotel"
 import { useAppContext } from "./contexts/AppContext"
+import MyHotels from "./pages/MyHotels"
 
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
         {isLoggedIn && (
           <>
             <Route path="/add-hotel" element={<Layout><AddHotel /></Layout>}/>
+            <Route path="/my-hotels" element={<Layout><MyHotels /></Layout>}/>
           </>)}
         <Route path="*" element={<Navigate to="/" />}/>
       </Routes>
