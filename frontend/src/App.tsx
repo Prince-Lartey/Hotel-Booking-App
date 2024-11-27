a640731773c42ba1a922c62cc30723b96cbc15a6
Prince-Lartey <lartey.princ@gmail.com>
2024-11-27 17:09:47 +0000

    Implemented the edit hotel functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn"
 import AddHotel from "./pages/AddHotel"
 import { useAppContext } from "./contexts/AppContext"
 import MyHotels from "./pages/MyHotels"
+import EditHotel from "./pages/EditHotel"
 
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
           <>
             <Route path="/add-hotel" element={<Layout><AddHotel /></Layout>}/>
             <Route path="/my-hotels" element={<Layout><MyHotels /></Layout>}/>
+            <Route path="/edit-hotel/:hotelId" element={<Layout><EditHotel /></Layout>}/>
           </>)}
         <Route path="*" element={<Navigate to="/" />}/>
       </Routes>
