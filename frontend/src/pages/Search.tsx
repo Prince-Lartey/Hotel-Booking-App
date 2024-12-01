6b580bc1141213231bc5309499084923f0ab60df
Prince-Lartey <lartey.princ@gmail.com>
2024-12-01 19:13:09 +0000

    Implemented the home page

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,13 +1,46 @@
+import { useState } from "react"
 import { useSearchContext } from "../contexts/SearchContext"
+import { useQuery } from "react-query"
+import * as apiClient from "../api-client"
+import SearchResultCard from "../components/SearchResultCard"
 
 
 const Search = () => {
     const search = useSearchContext()
-    console.log(search)
+    const [page, setPage] = useState<number>(1)
+
+    const searchParams = {
+        destination: search.destination,
+        checkIn: search.checkIn.toISOString(),
+        checkOut: search.checkOut.toISOString(),
+        adultCount: search.adultCount.toString(),
+        childCount: search.childCount.toString(),
+        page: page.toString(),
+    }
+
+    const { data: hotelData } = useQuery(["searchHotels", searchParams], () => apiClient.searchHotels(searchParams))
 
     return (
-        <div>
-            Search Page
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+            <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+                <div className="space-y-5">
+                    <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">FIlter by:</h3>
+                    {/* TODO FILTERS */}
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-5">
+                <div className="flex justify-between items-center">
+                    <span className="text-xl font-bold">
+                        {hotelData?.pagination.total} Hotels found {search.destination ? `in ${search.destination}` : ""}
+                    </span>
+                    {/*  TODO Sort options */}
+                </div>
+
+                {hotelData?.data.map((hotel) => (
+                    <SearchResultCard  hotel={hotel}/>
+                ))}
+            </div>
         </div>
     )
 }
